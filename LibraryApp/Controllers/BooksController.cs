eacd2b4d3a57fa433f3ee796c44ab4710a0cc37d
Daron Short <daron.short@hotmail.com>
2018-05-14 19:39:23 -0500

    Add project files.

---
+++ LibraryApp/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers {
+    public class BooksController : Controller {
+
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db) {
+            _db = db;
+        }
+
+        public IActionResult Index() {
+            return View(_db.Books.ToList());
+        }
+
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book) {
+            if (ModelState.IsValid) {
+                _db.Add(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                _db.Dispose();
+            }
+        }
+
+    }
+}
